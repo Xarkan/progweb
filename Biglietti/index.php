eea0be94a7de704918fbd9e46b79cc97313d2285
edgardovittoria <edgardovittoria@hotmail.it>
2018-04-19 12:09:02 +0200

    test su entity

--- Biglietti/index.php
+++ Biglietti/index.php
@@ -18,18 +18,22 @@
         echo $sport->getData();
         echo " ".$teatro->getNome();
         
-        $dbm = new FDBmanager();
-        echo $dbm->getError();
+        //$dbm = new FDBmanager();
+        //echo $dbm->getError();
         
-        /*$e1 = new EBiglietti_Zona("Live", "Platea", 13);      //test add/remove
+        $e1 = new EBiglietti_Zona("Live", "Platea", 13);      //test add/remove
         $e2 = new EBiglietti_Zona("Partita", "Prato", 50);
         $e3 = new EBiglietti_Zona("Opera","Galleria", 15);
         
         $array = array($e1,$e2,$e2,$e3);
         $ord = new EOrdine(1, "Pippo", $array, $data, false);
+        $utente = new EUtente_Reg("federico", "raparelli", "jebfbihb", "abc");
+        $utente->paga($ord);
+        echo var_dump($ord->getPagato());
+        echo $ord->calcolaPrezzo();
         
-        $ord->removeBigl($e2);
-        var_dump($ord->getLista_bigl()); */
+        //$ord->removeBigl($e2);
+        //var_dump($ord->getLista_bigl()); 
         
         ?>
         <h1>PROVA</h1>
