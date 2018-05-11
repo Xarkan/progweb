9ae6ed5ca3cfdc1ea10bf3ad693b3bba23c434f8
edgardovittoria <edgardovittoria@hotmail.it>
2018-05-11 12:26:47 +0200

    introduzione di smarty in dataluogoprezzo

--- Biglietti/Config.php
+++ Biglietti/Config.php
@@ -6,3 +6,13 @@ $config['mysql']['host'] = "localhost";
 $config['mysql']['user'] = "root";
 $config['mysql']['password'] = "";        
 $config['mysql']['database'] = "DB_biglietti";
+
+//configurazione smarty
+$config['smarty']['template_dir'] =
+'/Biglietti/templates/template/';
+$config['smarty']['compile_dir'] =
+'/Biglietti/templates/templates_c/';
+$config['smarty']['config_dir'] =
+'/Biglietti/templates/configs/';
+$config['smarty']['cache_dir'] =
+'/Biglietti/templates/cache/';
