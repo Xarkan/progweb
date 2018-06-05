c1fe90b24a5d63035b96969078748678cd888dd4
edgardovittoria <edgardovittoria@hotmail.it>
2018-06-05 16:24:57 +0200

    DataLuogoPrezzo funzionante

--- TicketStore/View/javascript/DataLuogoPrezzo.js
+++ TicketStore/View/javascript/DataLuogoPrezzo.js
@@ -21,19 +21,19 @@ function setTable(risposta) {
     var splitted_url = url.split("/");
     var codice = splitted_url[splitted_url.length - 1];
     
-    for(let i = 0; i < 1 ; i++) {
+    for(let i = 0; i < risposta[codice].eventi.length ; i++) {
         let html_command = '<tr><td>'+risposta[codice].eventi[i].data+'</td>'+
             '<td>'+risposta[codice].eventi[i].luogo.citta+'</td>'+
             '<td>'+risposta[codice].eventi[i].luogo.struttura+'</td>'+
             '<td>'+risposta[codice].eventi[i].luogo.via+'</td>'+
-            '<td>'+risposta[codice].eventi[i].partecipazioni[i].prezzo+'</td>'+
+            '<td>'+risposta[codice].eventi[i].partecipazioni[0].prezzo+'</td>'+
             '<td><a type="button" class="btn btn-warning" href="#">Acquista</a></td></tr>';
-        table += table + html_command;    
+        table = table + html_command;    
     }
     document.getElementById("table-section").innerHTML = table;
     let html_command_nome = '<h4>'+risposta[codice].nome+'</h4>';
     document.getElementById("nome").innerHTML = html_command_nome;
-    let html_command_immagine = '<img src='+risposta[codice].img+'class="img-fluid" alt="Responsive image">'
+    let html_command_immagine = '<img src=/TicketStore/'+risposta[codice].img+' class="img-fluid" alt="Responsive image">';
     document.getElementById("immagine").innerHTML = html_command_immagine;
 
 }   
