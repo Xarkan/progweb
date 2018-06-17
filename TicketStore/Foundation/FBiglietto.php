c48e0e2671db857cb30730e4c063490920a46284
Federico Raparelli <federico.raparelli@gmail.com>
2018-06-17 09:50:24 +0200

    finito Fbiglietto..manca il debug

--- TicketStore/Foundation/FBiglietto.php
+++ TicketStore/Foundation/FBiglietto.php
@@ -10,22 +10,35 @@ class FBiglietto extends FDBmanager{
         $sessione = USingleton::getInstance('USession');
         $posti = $sessione->recupera_valore('posti');
         //--------------da controllare---------------
-        $query_id = "SELECT LAST_INSERT_ID()"; 
+        $query_id = "SELECT LAST_INSERT_ID()"; //FROM ordine
         $result = $this->connection->query($query_id);
         $rows = $result->fetchAll();
         
         $id = $rows[0][0];
         //-------------------------------------------
-        
+        $part = $ordine->getItems();
+        $luogo = $ordine->getEvento()->getLuogo();
+        $citta = $luogo->getCitta();
+        $struttura = $luogo->getStruttura();
+        $indirizzo = $citta.", ".$struttura;
+        $updated = true;
+        for ($i = 0; $i < count($ordine->getItems()); $i++) {
+           
         $sql = "UPDATE biglietto SET codo=".$id.","     //codo mail fila posto
                 ." mail=".$this->connection->quote($ordine->getUtente()->getMail()).","
                 ." fila=".$this->connection->quote($posti[$i]->getFila()).","
                 ." posto=".$this->connection->quote($posti[$i]->getPosto()). " WHERE code="
-                .$this->connection->quote($ordine->getCode())." AND"
-                .$this->connection->quote($ordine->getItems());    //da finire!
+                .$this->connection->quote($ordine->getCode())." AND zona="
+                .$this->connection->quote($part[$i]->getZona()->getNome())." AND indirizzo="
+                .$this->connection->quote($indirizzo)." AND data_evento="
+                .$this->connection->quote($ordine->getEvento()->getData());    
         
         $affected_rows = $this->connection->exec($sql);
-        return $affected_rows > 0;        
+        if(!$affected_rows > 0) {
+            $updated = false;
+        }
+        }
+        return $updated;        
     }
     
     
