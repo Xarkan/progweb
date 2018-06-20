cf8f7b627af62ed71396140b5cc230559c67bdf2
edgardovittoria <edgardovittoria@hotmail.it>
2018-06-20 11:16:42 +0200

    conferma password

--- TicketStore/Control/CRegistrazione.php
+++ TicketStore/Control/CRegistrazione.php
@@ -11,41 +11,51 @@ class CRegistrazione {
         $cognome = $_POST['cognome'];
         $mail = $_POST['mail'];
         $password = $_POST['psw'];
+        $conferma_psw = $_POST['conferma_psw'];
         
         $utente = new EUtente_Reg($nome, $cognome, $mail, $password);
         
         $dominio = explode('@', $mail);
         if($dominio[1] != 'ticketstore.it') { 
-            if($mail != "" && $password != "" ){
-                $exist = $db->exist($utente);
-                if($exist){
-                 /*L'utente sta provando a registrarsi con una mail già usata per la registrazione
-                 Viene reindirizzato al login...l'ideale sarebbe comunicare all'utente che o si logga 
-                 o cambia mail per effettuare la registrazione
-                 header('HTTP/1.1 301 Moved Permanently');
-                 header('Location: login');*/
-                    echo '<script type="text/javascript">
-                        alert("La mail inserita è stata già registrata. Procedi al login")
-                        window.location= "/TicketStore/login"
-                      </script>'; 
+            if($mail != "" && $password != "" && $conferma_psw != ""){
+                if($password == $conferma_psw){
+                    $exist = $db->exist($utente);
+                    if($exist){
+                     /*L'utente sta provando a registrarsi con una mail già usata per la registrazione
+                     Viene reindirizzato al login...l'ideale sarebbe comunicare all'utente che o si logga 
+                     o cambia mail per effettuare la registrazione
+                     header('HTTP/1.1 301 Moved Permanently');
+                     header('Location: login');*/
+                        echo '<script type="text/javascript">
+                            alert("La mail inserita è stata già registrata. Procedi al login")
+                            window.location= "/TicketStore/login"
+                          </script>'; 
+                    }
+                    else{
+                        $registrato = $db->store($utente);
+                        if($registrato){
+                        //la registrazione è avvenuta con successo l'utente viene reinderizzato nella bellissima
+                        //pagina dove puo scegliere se andare alla home o procedere con l'ordine
+                            $sessione->imposta_valore('logged',$registrato);
+                            $sessione->imposta_valore('utente',$utente);
+                            header('HTTP/1.1 301 Moved Permanently');
+                            header('Location: ValidazioneUtente');
+                        }
+                   }
+                
                 }
                 else{
-                    $registrato = $db->store($utente);
-                    if($registrato){
-                    //la registrazione è avvenuta con successo l'utente viene reinderizzato nella bellissima
-                    //pagina dove puo scegliere se andare alla home o procedere con l'ordine
-                        $sessione->imposta_valore('logged',$registrato);
-                        $sessione->imposta_valore('utente',$utente);
-                        header('HTTP/1.1 301 Moved Permanently');
-                        header('Location: ValidazioneUtente');
-                    }
+                    echo '<script type="text/javascript">
+                               alert("Le password inserite non coincidono.")
+                               window.location= "/TicketStore/signin"
+                             </script>';
                 }
-            }
          }else{
              echo '<script type="text/javascript">
                         alert("Il formato della mail inserita non è valido.")
                         window.location= "/TicketStore/signin"
                       </script>';
-         }
+          }
+        }
     }     
 }
