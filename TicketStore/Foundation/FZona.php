4d9d27154beea397b546bd8003f796146f665dd4
edgardovittoria <edgardovittoria@hotmail.it>
2018-06-22 23:38:26 +0200

    existzona

--- TicketStore/Foundation/FZona.php
+++ TicketStore/Foundation/FZona.php
@@ -8,14 +8,30 @@ class FZona extends FDBmanager {
     $sql = "INSERT INTO zona VALUES (?,?,?)";
     $statement = $this->connection->prepare($sql);
     
-    $statement->bindParam(1, $luogo->getZonaSingola($i)->getNome());
+    $statement->bindParam(1, $nome);
     $statement->bindParam(2, $indirizzo);
-    $statement->bindParam(3, $luogo->getZonaSingola($i)->getCapacita());
+    $statement->bindParam(3, $capacita);
+    
+    $nome = $luogo->getZonaSingola($i)->getNome();
+    $capacita = $luogo->getZonaSingola($i)->getCapacita();
     
 
     $stored = $statement->execute();
 
     return $stored;
     
+    }
+    public function existZona(ELuogo $luogo,$i=0) {
+    $indirizzo = $luogo->getCitta().", ".$luogo->getStruttura();    
+    $nome = $luogo->getZonaSingola($i)->getNome();
+    
+    $sql = "SELECT nome FROM zona WHERE nome = ? AND indirizzo = ? ";
+    $statement = $this->connection->prepare($sql);
+    $statement->bindParam(1, $nome);
+    $statement->bindParam(2, $indirizzo);
+    $statement->execute();
+    $result = $statement->fetchAll();
+    return count($result) > 0;
 }
+
 }
